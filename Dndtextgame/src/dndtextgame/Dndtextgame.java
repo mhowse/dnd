136dee9d065a56878569ae5156d3bacd2cc04e2b
mhowse <mikhaila.howse@gmail.com>
2015-07-09 16:25:25 +1200

    Comments begun.

---
+++ Dndtextgame/src/dndtextgame/Dndtextgame.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dndtextgame;
+
+/**
+ *This will be an emulation of roll your own adventure books. 
+ * 
+ * @author cleddyf
+ */
+public class Dndtextgame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
