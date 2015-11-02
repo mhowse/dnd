59799563215af815c48316a20beac33f52e90479
mhowse <mikhaila.howse@gmail.com>
2015-11-02 20:18:07 +1300

    D comments.

--- d/src/d/D.java
+++ d/src/d/D.java
@@ -2,10 +2,7 @@
      * D.java, 
      * Short for dungeons and dragons. 
      * A silly high fantasy interactive fictiony thing. 
-     */
-
-    /**
-     *
+     * November 2015
      * @author MHowse
      */
 
@@ -13,20 +10,29 @@
     
     
    
-
+/**
+ * Import graphics and swing libraries.
+ */
 import java.awt.Graphics.*;
 import javax.swing.*;
 
+    /**
+     * Declare class.
+     */
 public class D {
   
-  protected static int [] history = new int [10];
+    /**
+     * Declare and initialize class variables
+     */
+  protected static int [] history = new int [10]; 
+//keeps track of what pages have been visited in what order. 
   protected int pageNumber;
  
   //current options to consider before continuing,
   //make page object in binary tree? option 1/2 == branch right left,
   //back == go up tree  towards root. or keep history array?
   //history array allows for keeping track of dead end-backtracking adventures better
- protected  static int pageCount;
+ protected  static int pageCount; // how many pages through story we are
   protected  static int currentPage = history[pageCount]; //one currently on. 
   protected  static int prevoiusPage = history[pageCount-1];
   protected static int pageNumberValue;
@@ -59,6 +65,11 @@ public class D {
     pageTurn(Integer.parseInt(page)); //change pages; 
   }
   
+  /**
+   * pageTurn, keeps variables in correct state and calls 
+   * the methods required to make the page turn come into effect.
+   * @param int a : a is the page to be turned to 
+   */
   public  static void pageTurn( int a ){
     pageCount++;
     history[pageCount] = pageNumberValue; //record
@@ -78,19 +89,33 @@ public class D {
         page4();
         break;
       default:
-        
-        
+             
     }
   }
   
+  /**
+   *  Accessor method that says which page user is currently on. 
+   * In string form.
+   * @return string that is the page.
+   */
   public static String getPage(){
-    String p = ""+currentPage;
+    String p = Integer.toString(currentPage);
     return p;
   }
+  /**
+   *  Accessor method that says which page user is currently on. 
+   * In int form
+   * @return int  that is the page.
+   */
   public   static int currentPage(){
     return history[pageCount];
   }
   
+  /**
+   *  Accessor method that says which page user was  previuosly on. 
+   * In int form.
+   * @return in that is the page number.
+   */
   public  static int prevoiusPage(){
     if (pageCount ==0){
       return pageCount;
@@ -98,6 +123,9 @@ public class D {
     return history[pageCount-1];
   }
   
+  /**
+   * Initial page loaded thing. 
+   */
   public static void prolouge(){
     one = "This is a dnd inspired roll your own adventure interactive fiction";
     gui.drawPage(one);
@@ -105,21 +133,37 @@ public class D {
     pageCount = 0;
   }
   
+  /**
+   *  method for the first page,initialise the string that will be the page text
+   * then send it to the gui to be displayed. 
+   */
   public static void page1 (){ 
     //set page text field to the page. 
     one = "This is the begining of the story";
     gui.drawPage(one);
     //add some ascii art. 
   }
-  
+   /**
+   *  method for the second page,initialise the string that will be the page
+   * text
+   * then send it to the gui to be displayed. 
+   */
   public static void page2 (){
     String derp ="derp";
     //gui.displayString(derp);
   }
-  
+   /**
+   *  method for the third page,initialise the string that will be the page 
+   * text
+   * then send it to the gui to be displayed. 
+   */
   public static void page3(){
     
   }
+   /**
+   *  method for the fourth page,initialise the string that will be the page text
+   * then send it to the gui to be displayed. 
+   */
   public static void page4(){
     
   }
