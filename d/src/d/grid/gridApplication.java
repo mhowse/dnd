e61f0406b025e7d23a2a310d5b39a54ef5a4bd7e
mhowse <mikhaila.howse@gmail.com>
2016-06-14 22:14:01 +1200

    Button functionality,and a few other things tidied up.

--- d/src/d/grid/gridApplication.java
+++ d/src/d/grid/gridApplication.java
@@ -6,12 +6,12 @@ package d.grid;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.PopupMenu;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
 import javax.swing.*;
+import java.lang.Object.*;
 
 
-public class gridApplication extends JFrame{
+public class gridApplication extends JFrame  {
     private JFrame frame;
     private Timer timer;
     public gridApplication(){
@@ -34,6 +34,13 @@ public class gridApplication extends JFrame{
     }
     
     /**
+     * Stops or pauses the game when the stop button is clicked. 
+     */
+    public void stop(){
+        
+    }
+    
+    /**
      * Makes components and sticks them in place. 
      * Handles the gui creation basically. 
      */
@@ -42,12 +49,25 @@ public class gridApplication extends JFrame{
 		frame = new JFrame("gridGame");
                 JButton stop = new JButton("Stop");
                 JButton start = new JButton("Start");
+                stop.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        stop();
+                    }
+                });
+                start.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        start();
+                    }
+                });
+                               
                 JPanel ctrlP = new JPanel();//control panel
                 ctrlP.setPreferredSize(new Dimension(500, 100));
                 ctrlP.add(start);
                 ctrlP.add(stop);
                 ctrlP.setBackground(Color.red);
-                frame.setPreferredSize(new Dimension(500,600));
+                frame.setPreferredSize(new Dimension(500,500));
                 frame.setContentPane(ctrlP);
 		frame.getContentPane().add(Map);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
